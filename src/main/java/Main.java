a9069e4c02077970d5f9d978a141bbfdee8b4d74
OceanHL <876474290@qq.com>
2023-10-09 21:25:02 +0800

    初始化

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: Main
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author Oceanj_hl
+ * @Create 2023/8/23 19:35
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
